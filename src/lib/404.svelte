b3836cc4b2bba248e275f8e838d953d050b2c50e
Duquerson <sotodjgato@gmail.com>
2023-06-26 16:49:57 -0400

    404 page

--- src/lib/404.svelte
+++ src/lib/404.svelte
@@ -1,1 +1,22 @@
-<script></script>
+
+<figure>
+	<img src="./src/assets/bg-404.webp" alt="404 page not found" >
+	<figcaption>404 not found</figcaption>
+</figure>
+
+<style>
+	figure{
+		display: flex;
+		flex-direction: column;
+	}
+	figcaption{
+		color:hsl(0, 0%, 52%);
+		text-align: center;
+		font-size: 70px;
+		font-weight: 800;
+		background-color: transparent;
+		z-index: 1;
+		margin-top: -38%;
+
+	}
+</style>
