eb3669d7e8dcc0de723bc75a3b42ae86d8eee1b0
Duquerson <sotodjgato@gmail.com>
2023-06-27 22:15:48 -0400

    cambios al search 1

--- src/lib/List_search.svelte
+++ src/lib/List_search.svelte
@@ -3,11 +3,11 @@
 	import Search from './Search.svelte';
 </script>
 
-<section class="text-color-light  dark:text-text-color-dark h-[90px] bg-menu-color-light dark:bg-menu-color-dark ">
-	<div class="">
+<section class="h-[100px] bg-menu-color-light dark:bg-color-dark flex flex-row justify-between">
+	<div class="text-color-light lg:w-[460px] dark:text-text-color-dark h-[60px] flex content-center min-w-[346px] bg-white dark:bg-menu-color-dark rounded-lg shadow-lg ">
 		<Search/>
 	</div>
-	<div class="">
+	<div class="list">
 		<ListContinets/>
 	</div>
 </section>
@@ -16,8 +16,10 @@
 	section{
 		display: flex;
 		flex-direction: row;
-		padding-top: 10px;
+		padding-top: 20px;
 		padding-left: 16px;
 		padding-right: 16px;
 	}
+
+
 </style>
--- src/lib/Search.svelte
+++ src/lib/Search.svelte
@@ -1,11 +1,10 @@
 <script>
-    import {createEventDispatcher} from 'svelte';
 	import {API} from './readableStore';
 
     let value = '';
     const searchValue = async (value)=>{
             try {
-				const promise = await fetch(`${API}${value}`);
+				const promise = await fetch(`${$API}name/${value}`);
 				if(!promise.ok){
 					throw new Error(`Error ${promise.status}: Failed Connection to Server`);
 				}
@@ -17,17 +16,14 @@
         };
     const search = (event)=>{
         value = event.target.value;
-        if(value.length > 2 ){
+        if(value.length > 1 ){
             searchValue(value);
         }
     }
 
 </script>
 
-
-<div class="text-color-light lg:w-[580px] dark:text-text-color-dark h-[59px] flex content-center mb-[47px] min-w-[343px] bg-white dark:bg-menu-color-dark rounded-lg shadow-lg ">
-    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 my-auto mx-9 ">
+    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 my-auto mx-6 ">
         <path stroke-linecap="round" stroke-linejoin="round" d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z" />
     </svg>
-    <input on:keydown={search} class="w-[70%]  dark:bg-menu-color-dark dark:text-text-color-dark dark:placeholder-text-color-dark" type="search" name="search" id="search" placeholder="Search for a country...">
-</div>
+    <input on:keydown={search} class="lg:w-[365px] w-[260px] px-2 dark:bg-menu-color-dark dark:text-text-color-dark dark:placeholder-text-color-dark" type="search" name="search" id="search" placeholder="Search for a country...">
