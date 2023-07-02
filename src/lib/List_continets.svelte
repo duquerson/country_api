8d953130e13f1eab80fcaf8e969fef3af8dd1e1e
Duquerson <sotodjgato@gmail.com>
2023-07-02 14:48:34 -0400

    cambios menores a la lsita de seleccion de continentes

--- src/lib/List_continets.svelte
+++ src/lib/List_continets.svelte
@@ -1,24 +1,21 @@
 
 <script>
 
-    import {createEventDispatcher} from 'svelte';
-
-    export let continents = [];
+    let continents = ['Africa', 'America', 'Asia', 'Europa', 'Oceania'];
     let listOpen = false;
-    let select;
-    const dispatch = createEventDispatcher();
+    let select = '';
+
     const toggle = ()=>{
         listOpen = !listOpen;
     }
-    const listClick = (continents)=>{
-        select = continents
-        dispatch('continent', select);
+    const selectContinent = (continent)=>{
+        select = continent
         toggle();
     }
 </script>
 
-<div class="flex flex-nowrap justify-between">
-    <button on:click={toggle} class=" text-sm p-3 mt-2">
+<section class="flex flex-row flex-nowrap justify-between dark:text-white">
+    <button on:click={toggle} class="text-sm p-3 mt-2">
         <span  class="flex">
             {#if select}
                 {select}
@@ -27,18 +24,19 @@
             {/if}
         </span>
     </button>
-    <svg  xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 mt-5 mr-2" class:rotate-180={listOpen}>
-        <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 8.25l-7.5 7.5-7.5-7.5" />
-    </svg>
-</div>
+    <div>
+        <svg  xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6" class:rotate-180={listOpen}>
+            <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 8.25l-7.5 7.5-7.5-7.5" />
+        </svg>
+    </div>
+</section>
 {#if listOpen}
-    <ul class="w-[210px] mt-4 bg-white dark:bg-menu-color-dark flex flex-col rounded-lg shadow-lg">
+    <ul class="w-[210px] mt-4 bg-white dark:bg-menu-color-dark dark:text-white flex flex-col rounded-lg shadow-lg">
         {#each continents as continent}
-            <button on:click={()=>listClick(continent)}>
+            <button on:click={()=>selectContinent(continent)}>
                 <li class="text-left hover:bg-color-light hover:bg-opacity-5 hover:dark:bg-color-dark hover:rounded-lg p-4 text-sm">{continent}</li>
             </button>
         {/each}
     </ul>
 {/if}
 
-    
