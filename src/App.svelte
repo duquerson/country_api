ed8ab7d72ba3b00822826f53e41ff33f1a1a0924
Duquerson <sotodjgato@gmail.com>
2023-07-30 10:32:38 -0400

    cambios en la logica y manjeo de evento de busqueda

--- src/App.svelte
+++ src/App.svelte
@@ -4,19 +4,21 @@
   import P404 from "./lib/404.svelte";
   import ListSearch from "./lib/List_search.svelte";
   import Body from "./lib/Body.svelte";
-
+  let country ='';
   let currentRoute = "";
   const handleRouteChange = () => {
     currentRoute = window.location.pathname;
   };
+  let data = (event)=>{country = event.detail.country}
   onMount(() => handleRouteChange());
+
 </script>
 
 <main>
   <Header />
   {#if currentRoute === "/"}
-	<ListSearch on:search/>
-	<!-- <Body/> -->
+	<ListSearch on:search={data}/>
+	<Body parameters={country}/>
   {:else if currentRoute === "/:search"}
     <p>search</p>
   {:else}
--- src/lib/Search.svelte
+++ src/lib/Search.svelte
@@ -1,27 +1,29 @@
 <script>
-	import {API} from './readableStore';
-	import QueryAPI from './Query';
+	import { API } from './readableStore.js';
 	import { createEventDispatcher } from 'svelte';
 	import Dompurify from 'dompurify';
+
     let value = '';
 	let country;
 	const dispatch = createEventDispatcher();
     const search = (event)=>{
 		value = Dompurify.sanitize(event.target.value);
-		if(value.length>3){
+		if(value.length > 3){
 			setTimeout(()=>{
-			country = QueryAPI(`${$API}name/${value}`);
+			country = `${$API}name/${value}`;
 			dispatch('search', {country: country});
-			}, 2500);
+			}, 200);
 		}
     }
 
 </script>
-<section class="h-[60px] flex flex-row max-w-[200px]">
+<div class="h-[60px] flex flex-row max-w-[200px]">
 	<div class="my-auto mx-6 ">
 		<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.8" stroke="currentColor" class="w-6 h-6">
 			<path stroke-linecap="round" stroke-linejoin="round" d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z" />
 		</svg>
 	</div>
-    <input on:input={search} class="lg:w-[365px] w-[250px] px-2 dark:bg-menu-color-dark dark:text-text-color-dark dark:placeholder-text-color-dark" type="search" name="search" id="search" placeholder="Search for a country...">
-</section>
+	<form>
+		<input bind:value on:input={search} class="lg:w-[365px] w-[250px] px-2 dark:bg-menu-color-dark dark:text-text-color-dark dark:placeholder-text-color-dark" type="search" name="search" id="search" placeholder="Search for a country...">
+	</form>
+</div>
