f2898080b943f4b91fd1ae7eaad6823ba0cebb37
Duquerson <sotodjgato@gmail.com>
2023-08-06 20:48:22 -0400

    pruebas en manejo de eventos

--- src/App.svelte
+++ src/App.svelte
@@ -4,31 +4,47 @@
   import P404 from "./lib/404.svelte";
   import ListSearch from "./lib/List_search.svelte";
   import Body from "./lib/Body.svelte";
+
+	//arreglar el router y rutas
   let country ='';
-  let value='';
-  let currentRoute = "";
+	let value = '';
+  let currentRoute = '';
+  let url='';
   const handleRouteChange = () => {
     currentRoute = window.location.pathname;
   };
-  let data = (event)=>{
+  const handleSearch = (event)=>{
 	country = event.detail.country;
 	value = event.detail.value;
-	window.location.pathname= value;
-}
-  onMount(() => handleRouteChange());
+	url = window.location.hash = value;
+	window.location.pathname = 'search';
+	window.history.pushState(null, null, 'search');
+	console.log(url);
+    handleRouteChange();
+  }
+	const handleList = (event)=>{
+		country = event.detail.country;
+		value = event.detail.value;
 
+		window.history.pushState(null, null, value);
+		//window.history.pushState(null, null, value);
+		handleRouteChange();
+	}
+  onMount(() => handleRouteChange());
 </script>
 
 <main>
   <Header />
   {#if currentRoute === "/"}
-	<ListSearch on:search={data} on:select={data}/>
-	<Body parameters={country}/>
-  {:else if currentRoute === "/:id"}
-    <p>search</p>
+    <ListSearch on:search={handleSearch} on:select={handleList} />
+    <Body parameters={country} />
+  {:else if currentRoute === '/search'}
+  	<ListSearch on:search={handleSearch} on:select={handleList} />
+    <Body parameters={country} />
   {:else}
     <P404 />
   {/if}
+
 </main>
 <style>
 	:global(body){
